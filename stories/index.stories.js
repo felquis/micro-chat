1d4b56ec993967659ce5084ec5c1bc0f34635195
Felquis Gimenes <frgformenton@gmail.com>
2018-01-19 18:28:33 -0200

    chore: add test

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,13 +6,13 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-// import Chat from '../components/Chat'
-// import ChatInput from '../components/ChatInput'
-// import ChatContainer from '../container/Chat'
+import Chat from '../components/Chat'
+import ChatInput from '../components/ChatInput'
+import ChatContainer from '../container/Chat'
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-// storiesOf('Chat', module)
-//   .add('Chat', () => <Chat messages={[{ message: 'Hello' }, { message: 'How are you?' }]} />)
-//   .add('ChatInput', () => <ChatInput onSubmit={action('Submit Message')} />)
-//   .add('ChatContainer', () => <ChatContainer />)
+storiesOf('Chat', module)
+  .add('Chat', () => <Chat messages={[{ message: 'Hello' }, { message: 'How are you?' }]} />)
+  .add('ChatInput', () => <ChatInput onSubmit={action('Submit Message')} />)
+  .add('ChatContainer', () => <ChatContainer />)
